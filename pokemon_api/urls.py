0dca4fe359da60eefeb64e642f84f5f126396339
Luidy-Michael <luidymichael8@gmail.com>
2026-08-30 19:02:39 -0300

    c:\Users\luidy\Downloads\pokedex-project\pokedex-monolitico\venv\Scripts\activate.bat

--- pokemon_api/urls.py
+++ pokemon_api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path("buscar_dados_pokemon/", views.buscar_pokemon, name='buscar_dados_pokemon'),
     path("salvar_pokemon/", views.salvar_pokemon, name="salvar_pokemon"),
     path("listar_pokemons/", views.listar_pokemons, name="listar_pokemons")
